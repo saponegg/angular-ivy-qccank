7d1518b3312ab8efeb8bae709f69c8d89e3f5544
saponegg <saponegg@gmail.com>
2021-12-20 16:38:34 +0100

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TestComponent } from './test/test.component';
 import { LolComponent } from './lol.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, TestComponent, LolComponent],
+  declarations: [AppComponent, HelloComponent, TestComponent, LolComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
