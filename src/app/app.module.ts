93c938c51d1ec07234f3a680a4e33d58418c4bf5
saponegg <saponegg@gmail.com>
2021-12-20 10:55:31 +0100

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { TestComponent } from './test.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
